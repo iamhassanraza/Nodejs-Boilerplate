f29405eb781458361349f0f6b5321c513d3681d8
iamhassanraza <m.h.raxa1@gmail.com>
2020-05-02 21:18:42 +0500

    node boilerplate

---
+++ Routes/Routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/hassan',(req,res)=> res.send("hassan"))
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,24 @@
+require("dotenv").config();
 
-const express = require('express'),
-    app = express(),
-    defaultRoute = require('./Routes/defaultRoute'),
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    morgan = require('morgan');
-    
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  cors = require("cors"),
+  morgan = require("morgan"),
+  AppRoutes = require('./Routes/Routes'),
+  mysql = require('mysql'),
+  db = require('./config/database')
 
-    app.use(bodyParser.json({limit: '50mb'}));
-    app.use(bodyParser());
-    app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("tiny"));
 
-    app.use(morgan('tiny'));
+app.get("/", (req, res) => res.status(200).send("SBS Express Server Running v1.1"));
 
-
-
-
-app.use('/',defaultRoute);
-require('./Routes/app.routes')(app);
-
+app.use("api/v1",AppRoutes)
 
 
-
-app.listen(process.env.PORT || 8081);
-exports = module.exports = app;
+app.listen(process.env.PORT || 8081, () =>
+  console.log("Server running successfully on Port", process.env.PORT)
+);
